c00d103292242293725ea75a2470c8e872f53d14
cs <cs@cstos.com>
2026-06-01 10:54:45 +0800

    fix: color for flet name format

--- src/utils/color.py
+++ src/utils/color.py
@@ -27,34 +27,35 @@ class ColorMeta(EnumMeta):
 
 
 class Color(Enum, metaclass=ColorMeta):
-    RED = ColorItem(name="RED", name_zh="红色", hex="#f44336", rgb=(244, 67, 54))
-    PINK = ColorItem(name="PINK", name_zh="粉色", hex="#e91e63", rgb=(233, 30, 99))
-    PURPLE = ColorItem(name="PURPLE", name_zh="紫色", hex="#9c27b0", rgb=(156, 39, 176))
+    RED = ColorItem(name="red", name_zh="红色", hex="#f44336", rgb=(244, 67, 54))
+    PINK = ColorItem(name="pink", name_zh="粉色", hex="#e91e63", rgb=(233, 30, 99))
+    PURPLE = ColorItem(name="purple", name_zh="紫色", hex="#9c27b0", rgb=(156, 39, 176))
+    # NOTE: dont need use _ in name
     DEEPPURPLE = ColorItem(
-        name="DEEP_PURPLE", name_zh="深紫色", hex="#673ab7", rgb=(103, 58, 183)
+        name="deeppurple", name_zh="深紫色", hex="#673ab7", rgb=(103, 58, 183)
     )
-    INDIGO = ColorItem(name="INDIGO", name_zh="靛蓝", hex="#3f51b5", rgb=(63, 81, 181))
-    BLUE = ColorItem(name="BLUE", name_zh="蓝色", hex="#2196f3", rgb=(33, 150, 243))
+    INDIGO = ColorItem(name="indigo", name_zh="靛蓝", hex="#3f51b5", rgb=(63, 81, 181))
+    BLUE = ColorItem(name="blue", name_zh="蓝色", hex="#2196f3", rgb=(33, 150, 243))
     LIGHTBLUE = ColorItem(
-        name="LIGHT_BLUE", name_zh="浅蓝色", hex="#03a9f4", rgb=(3, 169, 244)
+        name="lightblue", name_zh="浅蓝色", hex="#03a9f4", rgb=(3, 169, 244)
     )
-    CYAN = ColorItem(name="CYAN", name_zh="青色", hex="#00bcd4", rgb=(0, 188, 212))
-    TEAL = ColorItem(name="TEAL", name_zh="蓝绿色", hex="#009688", rgb=(0, 150, 136))
-    GREEN = ColorItem(name="GREEN", name_zh="绿色", hex="#4caf50", rgb=(76, 175, 80))
+    CYAN = ColorItem(name="cyan", name_zh="青色", hex="#00bcd4", rgb=(0, 188, 212))
+    TEAL = ColorItem(name="teal", name_zh="蓝绿色", hex="#009688", rgb=(0, 150, 136))
+    GREEN = ColorItem(name="green", name_zh="绿色", hex="#4caf50", rgb=(76, 175, 80))
     LIGHTGREEN = ColorItem(
-        name="LIGHT_GREEN", name_zh="浅绿色", hex="#8bc34a", rgb=(139, 195, 74)
+        name="lightgreen", name_zh="浅绿色", hex="#8bc34a", rgb=(139, 195, 74)
     )
-    LIME = ColorItem(name="LIME", name_zh="黄绿色", hex="#cddc39", rgb=(205, 220, 57))
-    YELLOW = ColorItem(name="YELLOW", name_zh="黄色", hex="#ffeb3b", rgb=(255, 235, 59))
-    AMBER = ColorItem(name="AMBER", name_zh="琥珀色", hex="#ffc107", rgb=(255, 193, 7))
-    ORANGE = ColorItem(name="ORANGE", name_zh="橙色", hex="#ff9800", rgb=(255, 152, 0))
+    LIME = ColorItem(name="lime", name_zh="黄绿色", hex="#cddc39", rgb=(205, 220, 57))
+    YELLOW = ColorItem(name="yellow", name_zh="黄色", hex="#ffeb3b", rgb=(255, 235, 59))
+    AMBER = ColorItem(name="amber", name_zh="琥珀色", hex="#ffc107", rgb=(255, 193, 7))
+    ORANGE = ColorItem(name="orange", name_zh="橙色", hex="#ff9800", rgb=(255, 152, 0))
     DEEPORANGE = ColorItem(
-        name="DEEPORANGE", name_zh="深橙色", hex="#ff5722", rgb=(255, 87, 34)
+        name="deeporange", name_zh="深橙色", hex="#ff5722", rgb=(255, 87, 34)
     )
-    BROWN = ColorItem(name="BROWN", name_zh="棕色", hex="#795548", rgb=(121, 85, 72))
-    GREY = ColorItem(name="GREY", name_zh="灰色", hex="#9e9e9e", rgb=(158, 158, 158))
+    BROWN = ColorItem(name="brown", name_zh="棕色", hex="#795548", rgb=(121, 85, 72))
+    GREY = ColorItem(name="grey", name_zh="灰色", hex="#9e9e9e", rgb=(158, 158, 158))
     BLUEGREY = ColorItem(
-        name="BLUE_GREY", name_zh="蓝灰色", hex="#607d8b", rgb=(96, 125, 139)
+        name="bluegray", name_zh="蓝灰色", hex="#607d8b", rgb=(96, 125, 139)
     )
 
     @property
